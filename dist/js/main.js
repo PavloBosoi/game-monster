85725c840be9c766acbd2d4b4fd16cd10b850e3c
dark1910 <pashabos19@mail.ru>
2017-05-23 18:52:15 +0300

    view info

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,6 +6,9 @@ function ready(){
         el: '#app-monster',
         data: {
             showBar: false,
+            special: false,
+            display : [],
+            result: '',
             health: [
                 {
                     name: 'you',
@@ -14,6 +17,21 @@ function ready(){
                 },
                 {
                     name: 'monster',
+                    line: 100,
+                    statusGame: ''
+                },
+                {
+                    name: 'monster2',
+                    line: 100,
+                    statusGame: ''
+                },
+                {
+                    name: 'monster3',
+                    line: 100,
+                    statusGame: ''
+                },
+                {
+                    name: 'monster4',
                     line: 100,
                     statusGame: ''
                 }
@@ -28,14 +46,29 @@ function ready(){
                 for(var i = 0; i < this.health.length; i++){
                     this.health[i].line = 100;
                     this.health[i].statusGame = '';
+                    this.result = '';
                 }
             },
-            attack: function(event){
+            attack: function(event,special){
                 event.preventDefault();
-                var curRandom = this.getRandomArbitrary(5, 15);
+                special = special === undefined ? false : true;
+
+
+                var curRandom = 0;
                 var c = 0;
+
+                var display = [];
                 for(var i = 0; i < this.health.length; i++){
-                    this.health[i].line -= this.getRandomArbitrary(5, 15);
+                    var displayInfo = {};
+                    if(special === true){
+                        curRandom = this.getRandomArbitrary(10, 25);
+                        this.health[i].line -= curRandom;
+                    }
+                    else{
+                        curRandom = this.getRandomArbitrary(5, 10);
+                        this.health[i].line -= curRandom;
+                    }
+
                     /*this.health[i].line = this.health[i].line <= 0 ? 0 : this.health[i].line;
                      return this.health[i].name += ' - lose';*/
                     if(this.health[i].line <= 0){
@@ -50,8 +83,8 @@ function ready(){
                                 this.health[j].statusGame += ' win';
                             }
                         }
-                        console.log(c);
-                        if(c+1 === this.health.length){
+
+                        if(c+1 === this.health.length){//если с=4 значит счетчик дошел до последнего элемента и все предыдущие равны 0, если и последний элемент равен нулю, то ничья
                             if(this.health[c].line <= 0){
                                 for(var b = 0; b < this.health.length; b++){
                                     this.health[b].statusGame = '';
@@ -61,7 +94,34 @@ function ready(){
                         }
                         c++;
                     }
+
+                    displayInfo.hurt = curRandom;
+                    displayInfo.name = this.health[i].name;
+
+
+                    display.push(displayInfo);
                 }
+                this.display = display;
+                this.info();
+            },
+            heal: function() {
+
+            },
+            info: function() {
+                if(this.display.length != 0){
+                    this.result = '';
+                    for(var i = 0; i < this.display.length; i++){
+                        this.result += '<span style="background-color: '+this.getRandomColor()+'">Name: '+this.display[i].name+'<br/>Hurt: '+this.display[i].hurt+'</span>';
+                    }
+                }
+            },
+            getRandomColor: function() {
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
             }
         }
     });
